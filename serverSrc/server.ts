588d02b9776844783bb02905f8872e96f8e35d1c
BennEntterprise <kyle@bennentterprise.com>
2020-12-17 22:57:39 +0000

    Add CRA to client Side

--- serverSrc/server.ts
+++ serverSrc/server.ts
@@ -1,1 +1,24 @@
-console.log('Its working');
+import express from 'express'
+console.log('Its working')
+const PORT = process.env.NODE_ENV || 8080
+const HOST = process.env.HOST || 'localhost'
+
+// Obtain Environment Configs
+const app = express();
+
+
+//Connecct to Mongo Database
+
+
+//Import Routes/Routers
+
+
+// Mount routers
+
+
+
+// Open a Port
+app.listen(PORT, ()=>{
+    console.log(`The project is running on https:${HOST}:${PORT}`)
+    console.log(`The Project is running in ${process.env.NODE_ENV} mode.`)
+})
