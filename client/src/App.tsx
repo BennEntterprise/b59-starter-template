dd3802f5aa5ac9d126553fc9ddcb90f769b77efc
BennEntterprise <kyle@bennentterprise.com>
2020-12-18 09:51:47 +0000

    Test the /ping route in from the client code

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import axios, { AxiosError } from 'axios'
+import logo from './logo.svg'
+import './App.css'
 
-function App() {
+const App = () => {
+  const [pingSuccess, setPingSuccess] = useState(false)
+  useEffect(() => {
+    axios
+      .get('/ping')
+      .then((res: any) => setPingSuccess(res.data.success))
+      .catch((err: AxiosError) => console.error(err))
+  }, [])
+  const connectionMessage: string = `Connection to the server? ${
+    pingSuccess ? 'successful!' : 'unsuccessful :( '
+  }`
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           Learn React
         </a>
+
+        <h6>{connectionMessage}</h6>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
