355f57b1e0372245c217b867562d35e8f2f18138
BennEntterprise <kyle@bennentterprise.com>
2020-12-18 09:50:12 +0000

    build basic server.ts
    it has a /ping route respond with "pong"
    it serves a build client app from the client folder.

--- serverSrc/server.ts
+++ serverSrc/server.ts
@@ -1,10 +1,11 @@
-import express from 'express'
-console.log('Its working')
+import express, { Request, Response } from 'express'
+import path from 'path'
 const PORT = process.env.NODE_ENV || 8080
 const HOST = process.env.HOST || 'localhost'
+const NODE_ENV = process.env.NODE_ENV || 'unset'
 
 // Obtain Environment Configs
-const app = express();
+const app: express.Application = express();
 
 
 //Connecct to Mongo Database
@@ -16,9 +17,25 @@ const app = express();
 // Mount routers
 
 
+// Mount Middlewares
+// the __dirname is the current directory from where the script is running
+app.use(express.json())
+app.use(express.static(__dirname))
+app.use(express.static(path.resolve(__dirname,'../', 'client', 'build')))
+
+app.get('/ping', (req:express.Request, res: express.Response)=>{
+    res.send({msg: 'pong', success: true})
+})
+
+// Serve The Client Side React App
+app.get('/*', (req: express.Request, res: express.Response)=>{
+    let clientAppPath = path.resolve(__dirname,'../', 'client', 'build', 'index.html')
+    res.sendFile(clientAppPath)
+    console.log(`sending ${clientAppPath}`)
+})
 
 // Open a Port
 app.listen(PORT, ()=>{
-    console.log(`The project is running on https:${HOST}:${PORT}`)
-    console.log(`The Project is running in ${process.env.NODE_ENV} mode.`)
+    console.log(`The project is running on http://${HOST}:${PORT}`)
+    console.log(`The Project is running in ${NODE_ENV} mode.`)
 })
